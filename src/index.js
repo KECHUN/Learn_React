68f665e219cab2f5c78468b290a8530996b19879
KECHUN <kechunye@gmail.com>
2023-04-14 14:15:27 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello world</h1>
+    <p>You can change the world</p>
+  </div>,
+  document.getElementById("root")
+);
